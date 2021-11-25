b114a278c09556f2cd661f02ac133dd33e5e5f9b
unknown <milana2005.kv@gmail.com>
2021-11-25 21:08:22 +0100

    add components to project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import "./App.css";
 import Button from "@mui/material/Button";
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
+import Card from "./components/Card/Card";
+import Hero from "./components/Hero/Hero";
+import Main from "./components/Main/Main";
+
 function App() {
   return (
     <div className="App">
-      <h1>hey</h1>
+      <Navbar />
+      <Hero />
       <Button variant="contained">Hello World</Button>
+      <Main />
+      <Card />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from "./Navbar.module.css";
+const Navbar = () => {
+  return (
+    <div className={styles.NavbarContent}>
+      <h1>Hello from Navbar</h1>
+    </div>
+  );
+};
+
+export default Navbar;
