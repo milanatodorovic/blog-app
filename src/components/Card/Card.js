50b338bc518880ebb73876a1b770410c450b5c1c
unknown <milana2005.kv@gmail.com>
2021-11-30 20:14:22 +0100

    add bootstrap to project

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,11 +1,24 @@
 import React from "react";
-import styles from "./Card.module.css";
-const Card = () => {
+//import styles from "./Card.module.css";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+
+const CardComponent = () => {
   return (
-    <div className={styles.CardContent}>
-      <h1>Hello from Card</h1>
-    </div>
+    <>
+      <Card style={{ width: "18rem" }}>
+        <Card.Img variant="top" src="holder.js/100px180" />
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+      </Card>
+    </>
   );
 };
 
-export default Card;
+export default CardComponent;
